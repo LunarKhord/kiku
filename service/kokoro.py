d9401c1c954b9843818c56259f0580e31f92fb1c
LunarKhord <bugfix.exe@gmail.com>
2026-04-02 16:47:56 +0100

    (chore): Cleaned kokoro, a need to redo and move to API

--- service/kokoro.py
+++ service/kokoro.py
@@ -1,30 +1,17 @@
 import soundfile as sf
-from kokoro_onnx import Kokoro as KokoroEngine # Aliasing to avoid collision
-import asyncio
-
+import numpy as np
+from kokoro_onnx import Kokoro as KokoroEngine
+from typing import List, Dict
+import os
 
 
 class VoiceSynthesizer:
+    """
+    """
+
     def __init__(self, model_path: str, voices_path: str):
-        # Initializing the 'Acoustic Latent Space'
-        self.model = KokoroEngine(model_path, voices_path)
+        print("🔊 Initializing Kokoro Engine...")
+        self.engine = KokoroEngine(model_path, voices_path)
+        self.sample_rate = 24000  # Kokoro standard
+    
 
-    async def generate_speech(self, text: str, output_path: str, voice: str = "bm_george"):
-        """
-        Transmutes text into an auditory waveform using the INT8 engine.
-        """
-        # We use 'to_thread' because local TTS is a blocking CPU task
-        samples, sample_rate = await asyncio.to_thread(
-            self.model.create, 
-            text, 
-            voice=voice, 
-            speed=0.9, 
-            lang="en-us"
-        )
-
-        await self.save_to_disk(samples, sample_rate, output_path)
-       
-    async def save_to_disk(self, samples, sample_rate, path: str):
-        # Persisting the auditory artifact
-        await asyncio.to_thread(sf.write, path, samples, sample_rate)
-        print(f"Synthesis complete: {path}")
